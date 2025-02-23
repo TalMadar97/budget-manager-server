eba49d55b41c863c1d9630170c4a42fad43d4fc4
TalMadar97 <talmadar360@gmail.com>
2025-02-23 10:10:49 +0200

    Createing user

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -11,6 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Budget Manager API is running...");
